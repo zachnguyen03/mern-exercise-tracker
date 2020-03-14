688fa847f6c3d7a9c18bf237838d3a07370a7e68
Anh Nguyen <quanhwalker1999@gmail.com>
2020-03-14 23:40:07 +0200

    Finished backend

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -40,15 +40,17 @@ router.route('/:id').delete((req, res) => {
 router.route('/update/:id').post((req, res) => {
     Exercise.findById(req.params.id)
             .then(exercise => {
-                exercise.username = req.body.username;
-                exercise.description = req.body.description;
-                exercise.duration = Number(req.body.duration);
-                exercise.date = Date.parse(req.body.date);
+                exercise.username = req.body.username || exercise.username;
+                exercise.description = req.body.description || exercise.description;
+                exercise.duration = Number(req.body.duration) || exercise.duration;
+                exercise.date = Date.parse(req.body.date) || exercise.date;
 
                 exercise.save()
                         .then(() => res.json('Exercise updated!'))
                         .catch(err => res.status(400).json('Error: ' + err))
             }).catch(err => res.status(400).json('Error: ' + err))
 })
+
+
 
 module.exports = router;
